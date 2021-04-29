304c092a4223027ced910e983fb7185c5f7613e8
Gautam <gautam.dange2007@gmail.com>
2021-04-29 15:37:43 +0200

    initial commit for kakfa

---
+++ src/consumer.js
@@ -0,0 +1,82 @@
+const { Kafka } = require('kafkajs')
+const config = require('./config')
+
+const kafka = new Kafka({
+  clientId: config.kafka.CLIENTID,
+  brokers: config.kafka.BROKERS
+})
+
+const topic = config.kafka.TOPIC
+const consumer = kafka.consumer({
+  groupId: config.kafka.GROUPID
+})
+
+
+
+function prettyJSON(obj) {
+  console.log(JSON.stringify(obj, null, 2));
+}
+
+const run = async () => {
+  await consumer.connect()
+  await consumer.subscribe({ topic, fromBeginning: true })
+  await consumer.run({
+    eachMessage: async ({ message }) => {
+      try {
+        const jsonObj = JSON.parse(message.value.toString())
+        let passengerInfo = filterPassengerInfo(jsonObj)
+        if (passengerInfo) {
+
+          prettyJSON(passengerInfo)
+        }
+      } catch (error) {
+        console.log('err=', error)
+      }
+    }
+  })
+}
+
+function filterPassengerInfo(jsonObj) {
+  let returnVal = null
+
+  console.log(`eventId ${jsonObj.eventId} received!`)
+
+  if (jsonObj.event_type == "object-in") 
+  {
+    returnVal = jsonObj
+  }
+
+  return returnVal
+}
+
+run().catch(e => console.error(`[example/consumer] ${e.message}`, e))
+
+const errorTypes = ['unhandledRejection', 'uncaughtException']
+const signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2']
+
+errorTypes.map(type => {
+  process.on(type, async e => {
+    try {
+      console.log(`process.on ${type}`)
+      console.error(e)
+      await consumer.disconnect()
+      process.exit(0)
+    } catch (_) {
+      process.exit(1)
+    }
+  })
+})
+
+signalTraps.map(type => {
+  process.once(type, async () => {
+    try {
+      await consumer.disconnect()
+    } finally {
+      process.kill(process.pid, type)
+    }
+  })
+})
+
+module.exports = {
+  filterPassengerInfo
+}
